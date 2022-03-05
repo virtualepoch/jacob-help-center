282ae11755b9dcdfabcccc31ffa50e1bebd364fa
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-03-05 11:27:19 -0700

    3/5/22 update
    added to-top button
    re-styled page width

--- test.js
+++ test.js
@@ -2,9 +2,21 @@ var menubutton = document.getElementById("menu-button");
 var menubox = document.getElementById("menu-box");
 
 function menuOpen() {
-  if (menubox.className == "open"){
-      menubox.className = "";
-  }else{
-      menubox.className = "open";
+  if (menubox.className == "open") {
+    menubox.className = "";
+  } else {
+    menubox.className = "open";
+  }
+}
+
+window.onscroll = function () {
+  scroll();
+};
+
+function scroll() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("to-top-button").style.bottom = "30px";
+  } else {
+    document.getElementById("to-top-button").style.bottom = "-100px";
   }
 }
