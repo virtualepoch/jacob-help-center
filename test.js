8854a004749c47b54e82255d4a635b9866bf1ab9
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-03-06 18:15:53 -0700

    3-6-22 side nav update

--- test.js
+++ test.js
@@ -33,3 +33,63 @@ guides_nav_box.className = "";
 function scrollToTop(){
   window.scrollTo(0, 0);
 }
+
+var guide_1_pages = document.getElementById("guide-1-pages");
+var guide_2_pages = document.getElementById("guide-2-pages");
+var guide_3_pages = document.getElementById("guide-3-pages");
+var guide_4_pages = document.getElementById("guide-4-pages");
+var guide_5_pages = document.getElementById("guide-5-pages");
+var guide_6_pages = document.getElementById("guide-6-pages");
+
+function expandGuide1(){
+  if(guide_1_pages.style.display == ""){
+    guide_1_pages.style.display = "block";
+  }else{
+    guide_1_pages.style.display = "";
+  }
+}
+function expandGuide2(){
+  if(guide_2_pages.style.display == ""){
+    guide_2_pages.style.display = "block";
+  }else{
+    guide_2_pages.style.display = "";
+  }
+}
+function expandGuide3(){
+  if(guide_3_pages.style.display == ""){
+    guide_3_pages.style.display = "block";
+  }else{
+    guide_3_pages.style.display = "";
+  }
+}
+function expandGuide4(){
+  if(guide_4_pages.style.display == ""){
+    guide_4_pages.style.display = "block";
+  }else{
+    guide_4_pages.style.display = "";
+  }
+}
+function expandGuide5(){
+  if(guide_5_pages.style.display == ""){
+    guide_5_pages.style.display = "block";
+  }else{
+    guide_5_pages.style.display = "";
+  }
+}
+function expandGuide6(){
+  if(guide_6_pages.style.display == ""){
+    guide_6_pages.style.display = "block";
+  }else{
+    guide_6_pages.style.display = "";
+  }
+}
+
+var articlesBox = document.getElementById("article-pages-box");
+
+function expandArticles(){
+  if(articlesBox.style.display ==""){
+    articlesBox.style.display = "block";
+  }else{
+    articlesBox.style.display = "";
+  }
+}
